0ca7dfe81cd013211390f12bbaf3e95b0be16461
Wesley Berry <wesleyberry52@gmail.com>
2020-02-18 14:02:09 -0600

    automated code

--- test/appTest.js
+++ test/appTest.js
@@ -1,25 +1,27 @@
 const assert = require('chai').assert;
 const app = require('../app');
 
-// Results
-let sayHelloResult = app.sayHello();
-let addNumbersResult = app.addNumbers(5, 5);
-
 // Tests
 describe('App', function() {
-    it('sayHello should return hello', () => {
-        assert.equal(sayHelloResult, 'hello');
+    describe('sayHello()', () => {
+        let sayHelloResult = app.sayHello();
+        it('sayHello should return hello', () => {
+            assert.equal(sayHelloResult, 'hello');
+        });
+    
+        it('sayHello should return type string', () => {
+            assert.typeOf(sayHelloResult, 'string');
+        });
     });
 
-    it('sayHello should return type string', () => {
-        assert.typeOf(sayHelloResult, 'string');
-    });
-
-    it('addNumbers should be above five', () => {
-        assert.isAbove(addNumbersResult, 5);
-    });
-
-    it('addNumbers should return a number', () => {
-        assert.typeOf(addNumbersResult, 'number');
+    describe('addNumbers()', () => {
+        let addNumbersResult = app.addNumbers(5, 5);
+        it('addNumbers should be above five', () => {
+            assert.isAbove(addNumbersResult, 5);
+        });
+    
+        it('addNumbers should return a number', () => {
+            assert.typeOf(addNumbersResult, 'number');
+        });
     });
 });
